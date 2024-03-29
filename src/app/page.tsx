302993706cb4dd7ecacdc1e83492dba39ab846db
Luciano Carvalho <carvalholuciano232@gmail.com>
2024-03-29 10:36:59 -0300

    construção da pagina de sobre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,16 @@ export default function Home() {
   return (
     <>
       <Header></Header>
-      <div className="flex flex-col justify-between h-full bg-lime-200">
-        <main className='flex items-center justify-center gap-14'>
-          <h1 className='text-center text-3xl'>
-            <span className='text-[#01705F]'>Olá</span>, Prazer em te ver aqui ! <br />
-            Venha conhecer um pouco <br /> sobre minha <span className='text-[#01705F]'>história e projetos</span>. 
-          </h1>
-          <Image src="/coding.png" alt='pessoa programando' width={350} height={350}></Image>
-        </main>
-      <Footer></Footer>
-      </div>
-      
+        <div className="flex flex-col justify-center h-screen -mt-[54px]">
+          <main className='flex items-center justify-center gap-14'>
+            <h1 className='text-center text-3xl'>
+              <span className='text-[#01705F]'>Olá</span>, Prazer em te ver aqui ! <br />
+              Venha conhecer um pouco <br /> sobre minha <span className='text-[#01705F]'>história e projetos</span>. 
+            </h1>
+            <Image src="/coding.png" alt='pessoa programando' width={350} height={350}></Image>
+          </main>
+        <Footer></Footer>
+        </div>
     </>
   );
 }
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Image from 'next/image';
+
+export interface IAppProps {
+}
+
+export default function App (props: IAppProps) {
+  return (
+    <>
+        <Header></Header>
+        <div className='flex flex-col justify-center h-screen -mt-[54px]'>
+            <div className='flex justify-center items-center gap-40'>
+                <Image src="/developer.png" width={320} height={320} alt='developer'></Image>
+                <p className='text-[#2C444E] w-1/3 text-justify text-lg'>
+                    Sou um estudante de Ciências da Computação e Desenvolvedor Fullstack. 
+                    Moro em uma cidade no interior da Bahia, sou apaixonado por tecnologia e 
+                    fascinado em como ela pode mudar a vida das pessoas, comecei 
+                    a me aprofundar na programação há 2 anos  e desde lá sigo evoluindo diariamente 
+                    através de cursos e projetos, é com muito esforço que procuro aperfeiçoar minhas habilidades, 
+                    almejando sempre a excelência profissional.
+                </p>
+            </div>
+            <Footer></Footer>
+        </div>
+        
+
+    </>
+  );
+}
